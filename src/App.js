f40fa8aee0374c30afca34149cb1c377d8c46620
Shamar Knibbs <shamar.knibbs@stu.bmcc.cuny.edu>
2019-06-16 22:26:11 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import './App.css';
+import Roll from './Roll';
+class App extends Component {
+  render(){
+
+  
+  return (
+    <div className="App">
+     <Roll />
+      
+    </div>
+  );
+}
+}
+
+export default App;
